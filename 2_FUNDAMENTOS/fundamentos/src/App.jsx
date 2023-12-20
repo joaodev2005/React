2188ce8a88c734e7b409a00c92b43978fe9d5015
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-19 23:43:21 -0300

    Hierarquia de componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import FirstComponent from './components/FirstComponent'
 import TemplateExpression from './components/TemplateExpression'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <h1>Fundamentos do React</h1>
         <FirstComponent/>
         <TemplateExpression/>
+        <MyComponent/>
       </div>
     </>
   )
