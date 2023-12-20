2b80f94ad0d1fad37da7638649d3f39d8e7ae261
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-19 22:46:56 -0300

    importando componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import FirstComponent from './components/FirstComponent'
 
 function App() {
 
   return (
     <>
       <div className='App'>
-
+        <h1>Fundamentos do React</h1>
+        <FirstComponent/>
       </div>
     </>
   )
