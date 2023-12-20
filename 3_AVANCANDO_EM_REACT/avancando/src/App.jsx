efaa45bcdb4d58daa33ad6ba83cf81d9bb773d5a
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-20 11:47:19 -0300

    inserindo img em public

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Avançando em React</h1>
+        <img src="/img.jpg" alt="Rua de alguma cidade com grandes predios" />
+      </div>
+    </>
+  )
+}
+
+export default App
