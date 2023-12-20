1ab3cf6c3ce30694e2d741c64a32dac715f03b18
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-20 00:03:01 -0300

    Evento de click - com função

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,8 +1,17 @@
 const Events = () => {
+
+    const handleClick = (e) => {
+        console.log(e)
+        console.log('executou')
+    }
+
     return (
         <div>
             <div>
                 <button onClick={() => console.log('teste')}>Clique aqui</button>
+            </div>
+            <div>
+                <button onClick={handleClick}>Clique aqui - com função</button>
             </div>
         </div>
     )
