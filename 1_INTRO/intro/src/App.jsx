bca6ab889ca4e176593c78cbb3e831fce529b1da
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-19 21:55:22 -0300

    ambiente pronto

--- 1_INTRO/intro/src/App.jsx
+++ 1_INTRO/intro/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
 
   return (
     <>
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+      <div className='App'>
+        <h1>Hello World</h1>
+        
+      </div>
     </>
   )
 }
