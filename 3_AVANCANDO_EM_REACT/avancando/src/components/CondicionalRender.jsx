1c153ff93d5824d120f8e362a4c9ab239e0f76a4
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-20 23:26:08 -0300

    Renderização condicional com else

--- 3_AVANCANDO_EM_REACT/avancando/src/components/CondicionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/CondicionalRender.jsx
@@ -2,10 +2,22 @@
 const CondicionalRender = () => {
     const x = 5
 
+    const name = 'Douglas'
+
     return (
         <div>
             <h3>Isso será exibido</h3>
             {x == 4 && <p>Se x for true sim!</p>}
+            <h3>Render Ternário</h3>
+            {name === 'Douglas' ?(
+                <div>
+                    <p>Olá Douglas</p>
+                </div>
+            ) : (
+                <div>
+                    <p>Nome não encontrado!</p>
+                </div>
+            )}
         </div>
     )
 }
