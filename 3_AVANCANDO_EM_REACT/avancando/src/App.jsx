b11a6d6d43f2b43534d8e29e758ca4991d0a7b17
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-21 00:09:07 -0300

    Componentes com map

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -6,20 +6,31 @@ import CondicionalRender from './components/CondicionalRender'
 import ShowUserName from './components/ShowUserName'
 import CarDetails from './components/CarDetails'
 
+const cars = [
+  { id: 1, brand: 'Ferrari', color: 'Amarelo', km: 0 },
+  { id: 2, brand: 'BMW', color: 'Verde', km: 330 },
+  { id: 3, brand: 'Mitsubishi', color: 'Preto', km: 320 },
+]
+
 function App() {
 
 
   return (
     <>
-      <div className='App' style={{paddingBottom: '500px'}}>
+      <div className='App' style={{ paddingBottom: '500px' }}>
         <h1>Avançando em React</h1>
         <img src="/img.jpg" alt="rua de alguma cidade" />
         <img src={night} alt="" />
-        <Data/>
-        <ListRender/>
-        <CondicionalRender/>
-        <ShowUserName name='Jailson'/>
-        <CarDetails brand='VW' km={999} color='Dourado'/>
+        <Data />
+        <ListRender />
+        <CondicionalRender />
+        <ShowUserName name='Jailson' />
+        <CarDetails brand='VW' km={999} color='Dourado' />
+        <CarDetails brand='Fiat' km={3239} color='Branco' />
+        <CarDetails brand='BMW' km={4098} color='Azul' />
+        {cars.map((car) => (
+          <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} />
+        ))}
       </div>
     </>
   )
