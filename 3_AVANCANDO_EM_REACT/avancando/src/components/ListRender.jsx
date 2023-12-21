1704a856e61792d552fd37a7c40644f44e3d5eaf
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-12-20 22:11:39 -0300

    Propriedade Key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -3,15 +3,42 @@ import { useState } from "react"
 const ListRender = () => {
     const [list] = useState(['Matheus', 'Pedro', 'Juca'])
 
+    const [itens] = useState(['TV', 'Geladeira', 'Microondas'])
+
+    const [games] = useState(['Baldurs Gate', 'Alan Wake 2', 'Super Mario Bros 3'])
+
+    const [users, setUsers] = useState([
+        { id: 1, name: 'João', age: 18 },
+        { id: 2, name: 'Champola', age: 60 },
+        { id: 3, name: 'Jamorango', age: 24 },
+    ])
+
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {itens.map((ele) => (
+                    <li>{ele}</li>
+                ))}
+            </ul>
+            <ul>
+                {games.map((el) => (
+                    <li>{el}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.name} - {user.age} anos
+                    </li>
                 ))}
             </ul>
         </div>
     )
 }
- 
-export default ListRender
+
+export default ListRender 
