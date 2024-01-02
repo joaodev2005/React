d970c9abb82d3f72a0d00880c8df3c0be0f96159
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-01-02 19:49:43 -0300

    textarea

--- 5_FORM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -2,9 +2,11 @@ import './MyForm.css'
 
 import { useState } from 'react'
 
-const MyForm = ({userName, userEmail}) => {
+const MyForm = ({ userName, userEmail }) => {
     const [name, setName] = useState(userName)
     const [email, setEmail] = useState(userEmail)
+
+    const [bio, setBio] = useState('')
 
     const handleName = (e) => {
         setName(e.target.value)
@@ -13,13 +15,14 @@ const MyForm = ({userName, userEmail}) => {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        console.log(name, email)
+        console.log(name, email, bio)
 
         setName('')
         setEmail('')
+        setBio('')
     }
 
-    console.log(name, email)
+    console.log(name, email, bio)
 
     return (
         <form onSubmit={handleSubmit}>
@@ -35,7 +38,7 @@ const MyForm = ({userName, userEmail}) => {
             </div>
             <label>
                 <span>E-mail:</span>
-                <input 
+                <input
                     type="email"
                     name="email"
                     id="email"
@@ -43,6 +46,18 @@ const MyForm = ({userName, userEmail}) => {
                     onChange={(e) => setEmail(e.target.value)}
                     value={email || ''}
                 />
+            </label>
+            <label>
+                <span>Bio:</span>
+                <textarea
+                    name="bio"
+                    id="bio"
+                    cols="30"
+                    rows="10"
+                    placeholder='Descrição'
+                    onChange={(e) => setBio(e.target.value)}
+                    value={bio}
+                ></textarea>
             </label>
             <input type="submit" value="Enviar" />
         </form>
