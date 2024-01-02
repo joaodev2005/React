f67c5b060529f64ab3bfea13927ca1fd23537b38
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-01-02 15:42:06 -0300

    criando um formulário

---
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -0,0 +1,15 @@
+import './MyForm.css'
+
+const MyForm = () => {
+    return (
+        <form>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name='name' placeholder='Digite seu nome' />
+            </div>
+            <input type="submit" value="Enviar" />
+        </form>
+    )
+}
+
+export default MyForm
