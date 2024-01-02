95a65167cdf166672a48264f5e92d6740d87c4fc
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-01-02 14:42:19 -0300

    CSS dinâmico

--- 4_CSS_REACT/css_react/src/App.jsx
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
-
+  const n = 21
 
   return (
     <>
@@ -10,7 +10,13 @@ function App() {
         <h1>CSS no React</h1>
         < MyComponent />
         <p>Conteúdo vazou para o App</p>
-        <p style={{color: 'pink', fontSize:'3rem', padding: '4rem', borderTop: '1px solid black'}}>Este elemento tem estilos inline</p>
+        <p style={{ color: 'pink', fontSize: '3rem', padding: '4rem', borderTop: '1px solid black' }}>Este elemento tem estilos inline</p>
+        <h2 style={n > 10 ? {color: 'purple'} : {color: 'yellow'}}>
+          CSS dinâmico
+        </h2>
+        <h2 style={n > 20 ? {color: 'cyan'} : {color: 'yellow'}}>
+          CSS dinâmico
+        </h2>
       </div>
     </>
   )
