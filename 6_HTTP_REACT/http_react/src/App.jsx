653ff7ad253ad19d7cb50ef08dc4df4b3bd77e59
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-01-09 18:59:15 -0300

    enviando dados

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
+
+const url = "https://localhost:3000/products"
+
 import './App.css'
 
 function App() {
+  const [procucts, setProducts] = useState({})
 
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      console.log(data)
+    }
+
+    getData( )
+
+  }, [])
 
   return (
     <>
