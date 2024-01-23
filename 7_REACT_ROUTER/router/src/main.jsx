c0f878742900eb2c44e7bad20e3901321c7ba27a
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-01-23 14:51:31 -0300

    Configurando o React router

--- 7_REACT_ROUTER/router/src/main.jsx
+++ 7_REACT_ROUTER/router/src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Contact from './routes/Contact.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "Contact",
+    element: <Contact />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ 7_REACT_ROUTER/router/src/routes/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Página de Contato</div>
+  )
+}
+
+export default Contact
