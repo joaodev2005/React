e33dd07adce137bcbc3f134c869aa78259b5b0c3
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-02-22 16:20:53 -0300

    useState com input

--- 9_HOOKS/hook/src/components/HookUseState.jsx
+++ 9_HOOKS/hook/src/components/HookUseState.jsx
@@ -4,12 +4,30 @@ const HookUseState = () => {
 
     const [name, setName] = useState("Nikola Tesla")
 
+    const [age, setAge] = useState(50)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(age)
+    }
+
     return (
         <div>
             <h2>useState</h2>
             <p>{name}</p>
             <button onClick={() => setName("Albert Einstein")}>Alterar</button>
             <button onClick={() => setName("Nikola Tesla")}>Alterar</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                />
+                <input type="submit" value="Alterar" />
+            </form>
+            <p>voce tem {age} anos</p>
+            <hr />
         </div>
     )
 }
